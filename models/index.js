5090c9f55315adc703c5c4e84e5875075af87969
Sellieee <Sellieee@hotmail.com>
2019-10-05 19:30:57 +1000

    modified config in index.js

--- models/index.js
+++ models/index.js
@@ -11,13 +11,10 @@ var config = require(__dirname + "/../config/config")[env];
 var db = {};
 
 if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable], {
-    host: "localhost",
-    dialect: "mysql"
-  });
+  var sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   var sequelize = new Sequelize(config.database, config.username, config.password, {
-    host: "localhost",
+    host: "JawsDB",
     dialect: "mysql"
   });
 }
