f7eec3cef042adda3f9a7fa5a1f65f5d832ba21e
Sellieee <Sellieee@hotmail.com>
2019-10-05 19:35:02 +1000

    modified config in index.js

--- models/index.js
+++ models/index.js
@@ -5,7 +5,7 @@ var path = require("path");
 var Sequelize = require("sequelize");
 var basename = path.basename(module.filename);
 require("dotenv").config();
-var env = process.env.NODE_ENV || "development";
+var env = process.env.NODE_ENV.trim() || "development";
 var config = require(__dirname + "/../config/config")[env];
 
 var db = {};
