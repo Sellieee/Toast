0b3da2a8c3dde936c57de8de02076ebcb96488d1
Sellieee <Sellieee@hotmail.com>
2019-10-05 19:04:41 +1000

    added 'config' to index.js

--- models/index.js
+++ models/index.js
@@ -11,7 +11,7 @@ var config = require(__dirname + "/../config/config")[env];
 var db = {};
 
 if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable]);
+  var sequelize = new Sequelize(process.env[config.use_env_variable], config);
   console.log(process.env);
 } else {
   var sequelize = new Sequelize(
